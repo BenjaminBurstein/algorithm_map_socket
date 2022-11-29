1048b193b1e876d04b13e7bb7fd7ae3ab2f590c6
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2022-11-29 12:07:05 +0100

    create distance function

--- front/components/Map.vue
+++ front/components/Map.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="h-screen">
-    <l-map :zoom="zoom" :center="[center.lat, center.lng]">
+  <div class="relative h-screen">
+    <l-map class="z-0" :zoom="zoom" :center="[destination.lat, destination.lng]">
       <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
       <l-marker
-        v-for="(marker, index) in markers"
+        v-for="(pos, index) in othersPositions"
         :key="index"
-        :lat-lng="[marker.lat, marker.lng]"
+              :lat-lng="[pos.lat, pos.lng]"
       />
     </l-map>
+     <div class="absolute top-0 z-40 text-center w-full">
+        <p>{{ selfDistance }}</p>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -16,13 +20,25 @@ export default {
   data() {
     return {
       zoom: 13,
-      center: { lat: 48.8935717, lng: 2.2243825 },
-      markers: [
-        { lat: 55.9464418, lng: 8.1277591 },
+      destination: {lat: 48.8935717, lng: 2.2243825 },
+      selfPosition: {lat: 48.8927042, lng: 2.2368843},
+      selfMarker: { lat: 48.8935717, lng: 2.2243825 },
+      othersPositions: [
         { lat: 48.8935717, lng: 2.2243825 },
         { lat: 48.8927042, lng: 2.2368843 },
       ],
+
     };
+  },
+  computed: {
+      selfDistance() {
+        const position = this.selfPosition;
+        const marker = this.selfMarker;
+        const destination = this.destination;
+        const distance1 = this.$helpers.getDistance(position.lat, position.lng, marker.lat, marker.lng);
+        const distance2 = this.$helpers.getDistance(marker.lat, marker.lng, destination.lat, destination.lng);
+        return distance1 + distance2;
+      }
   },
 };
 </script>
