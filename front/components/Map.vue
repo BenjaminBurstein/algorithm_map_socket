ad6949eda7a07b9c077c457cbfe6010cdcd1e64e
Dylan <dylanstienne@gmail.com>
2022-11-29 12:16:19 +0100

    starting map alert

--- front/components/Map.vue
+++ front/components/Map.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="relative h-screen">
-    <l-map class="z-0" :zoom="zoom" :center="[destination.lat, destination.lng]">
+    <l-map
+      class="z-0"
+      :zoom="zoom"
+      :center="[destination.lat, destination.lng]"
+    >
       <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-      <l-marker
+
+      <l-marker :lat-lng="[selfPosition.lat, selfPosition.lng]" />
+      <l-marker :lat-lng="[selfMarker.lat, selfMarker.lng]" />
+
+      <!-- <l-marker
         v-for="(pos, index) in othersPositions"
         :key="index"
-              :lat-lng="[pos.lat, pos.lng]"
-      />
+        :lat-lng="[pos.lat, pos.lng]"
+      /> -->
     </l-map>
-     <div class="absolute top-0 z-40 text-center w-full">
-        <p>{{ selfDistance }}</p>
-    </div>
+    <MapAlert
+      class="absolute top-0 z-40"
+      :selfPosition="selfPosition"
+      :selfMarker="selfMarker"
+      :destination="destination"
+    />
   </div>
-
 </template>
 
 <script>
@@ -20,25 +30,14 @@ export default {
   data() {
     return {
       zoom: 13,
-      destination: {lat: 48.8935717, lng: 2.2243825 },
-      selfPosition: {lat: 48.8927042, lng: 2.2368843},
+      destination: { time: "13:00", lat: 48.8935717, lng: 2.2243825 },
+      selfPosition: { lat: 48.8927042, lng: 2.2368843 },
       selfMarker: { lat: 48.8935717, lng: 2.2243825 },
-      othersPositions: [
+      /* othersPositions: [
         { lat: 48.8935717, lng: 2.2243825 },
         { lat: 48.8927042, lng: 2.2368843 },
-      ],
-
+      ], */
     };
-  },
-  computed: {
-      selfDistance() {
-        const position = this.selfPosition;
-        const marker = this.selfMarker;
-        const destination = this.destination;
-        const distance1 = this.$helpers.getDistance(position.lat, position.lng, marker.lat, marker.lng);
-        const distance2 = this.$helpers.getDistance(marker.lat, marker.lng, destination.lat, destination.lng);
-        return distance1 + distance2;
-      }
   },
 };
 </script>
---
+++ front/components/MapAlert.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center w-full">
+    <p>{{ selfDistance }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selfPosition: null,
+    selfMarker: null,
+    destination: null,
+  },
+  computed: {
+    selfDistance() {
+      const distance1 = this.$helpers.getDistance(
+        this.selfPosition.lat,
+        this.selfPosition.lng,
+        this.selfMarker.lat,
+        this.selfMarker.lng
+      );
+      const distance2 = this.$helpers.getDistance(
+        this.selfMarker.lat,
+        this.selfMarker.lng,
+        this.destination.lat,
+        this.destination.lng
+      );
+      return distance1 + distance2;
+    },
+  },
+};
+</script>
